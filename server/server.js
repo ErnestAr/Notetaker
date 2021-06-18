39d0e4284d894c862e9a1db61a2b0f8bdf9c6da8
ErnestAr <arutiunian.ernest@gmail.com>
2021-06-18 18:57:57 -0400

    get request

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-test 1  
+const fs = require("fs")
+const storage = require("../db/db.json")
+// const script = require("../public/assets/js/index.js")
+const express = require('express')
+const { v4: uuidv4 } = require('uuid');
+const app = express()
+const port = 8000
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/', function(req, res) {
+    res.sendFile("public\notes.html")
+});
+
+  app.get('/notes', (req, res) => res.sendFile());
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
+
+
